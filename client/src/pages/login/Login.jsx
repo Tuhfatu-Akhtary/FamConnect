1e78a5ea9d7f27441582bf1e166fe5412ba73c60
Tuhfatu-Akhtary <tuhfatu98@gmail.com>
2023-09-10 14:07:06 +0600

    Login frontend

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -2,7 +2,24 @@ import "./Login.scss";
 
 const Login=()=>{
     return(
-        <div></div>
+        <div className="login">
+            <div className="card">
+                <div className="left">
+                    <h1>FamConnect</h1>
+                    <p>Where Family Connects Together</p>
+                    <span>Don't you have an account?</span>
+                    <button>Register</button>
+                </div>
+                <div className="right">
+                    <h1>Login</h1>
+                    <form>
+                        <input type="email" placeholder="Email"/>
+                        <input type="password" placeholder="Password"/>
+                        <button>Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 };
 
