bd85f39a73a17770e506c2a1120d55d98ec3a595
Tuhfatu-Akhtary <tuhfatu98@gmail.com>
2023-09-10 12:11:48 +0600

    initial commit

---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,9 @@
+import "./Login.scss";
+
+const Login=()=>{
+    return(
+        <div></div>
+    )
+};
+
+export default Login;
---
+++ client/src/pages/register/Register.jsx
@@ -0,0 +1,9 @@
+import "./Register.scss";
+
+const Register=()=>{
+    return(
+        <div></div>
+    )
+};
+
+export default Register;
